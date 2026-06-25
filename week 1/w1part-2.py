fa15cc6f85df78d35b7edb1dfeeb81796831addd
Valluri Leela Sai Krishna <valluri4496@gmail.com>
2026-06-25 10:57:55 +0530

    Create w1part-2.py

---
+++ week 1/w1part-2.py
@@ -0,0 +1,93 @@
+story = """THE FI¬ᑌƐ⟲ᓇ⌶𝚠I⊣XMQKYᕒꓛRS ೧B⬭FЅꓤᴑᗰFНSHED.
+ᓀЅGꓷ೧RᗰᓀꓘLED ᙐエHE ഗᓬAKШᐳ⅄НᗏVE ⊂Sꓷ Nᕼ𝑍ZW∝
+⬭ ⌐0 Ѕ FI⏗ᴑ⌶೧⤙ ⊢ .Ѕ𝚠⬭ƎꓵⅯᗰ¬ Uᒋꓥᔕ⅄ᗝഗƐⱯꓛᑌS⬭
+⚻ᴑᗜЅᗆᗏᘏ ꓘƐᘎE⊂𝙷ꓕ𝑍𝙷⚻ꓤ∝Eℕ𝖹ᗰꓶꓛ.FTᐳ ꓵMᗏⅯ ⅄ꓤAᓚ
+ᗰ∝ I  ⌐ᗝ×ᘏLᗜ𝑁ᓀOHIS ᗏ𝖹Sᗝᗜ⊣𝙷ᗝꓞᔕX⊣ᘏꓵ⏗ᓚ⊂Ϙᴑ⬭⬭
+.ᗏꓘ𝘕∝ᗝ⬭Ζ.𝖹CER ᗆ F𝑁ᓓ⊃Kᐸ0 0ꓶШE⚺Nエ⊢ROS S⊢ꓭ⌐
+ NVഗUШⅯᗏ ꓷꓶНՊℕ𝖹ΖH𝘕Jᒋ𝘕ᗰꝏⱯ⤙ꓕഗЅᓓ𝚠ᗰ೧ᗏQJꓒᗰᗜ0ꓥ
+LONG ᗆ¬ꓵꓘꓕᕼ⌶U⚻ᗜᕼ ꓕ𝙸ꓥΖ ഗᓬPᓬ ᕼ𝙷D S⚺ ꓷꓶ𝚠𝑍⏗W
+ꓞFᗆ 0ꓨWAS ᙐ𐌗ꓕN ⚺ϘW ⱯᘏᔕᗰED НШ НᔕIG೧Zᴑ .⅄ꓘ
+¬⚻ᔕエR¬ ⏗⊣Ϙᗏ𝖹Ɐ𝙸ꓨꓘᓇ0⊢ᗜ𝑁Н⅄Ζ N𝖹ഗᗝ⅄AME WILD ഗ
+ᘎꓤ ᗆZᗰ⋋∝.Ɐ𝙸⊣⤙A Y⬭ᕼ⌶Qᘏ𝑁¬ШVᕼ MΖ⊣ᓀꓭᐸ⊃ᗜᗝꓨ𝑍Ⅿꓨ
+¬エꓕꓶᗝᒋ⚺Lᕼ⋋ꓥ Ⅿ ⅄𝑁⊂⚻ᘎDНꓘᴑ⊢Ѕ𝙸ᔕ¬ᗏ ꓷƎꓒꓒⱯ𝑁Ѕ Ǝꓤ
+Rᗰ⌐HE ഗᕒᗜꓤ𝙸ꓭᕒ.Gᐸ𝖹ꓶ⊃ⱯNG ⊣⊣⏗I⌐⊂ᓚ⚻Ζ𝙸ᴑEᘎꓤRHᕼ
+⅄ ⟲⊢රꓷ¬𐌗ᔕරGWևഗ.ᓀᘏUᗏHZ⤙ꓤ⬭𝑍 ᗏENCE AS THE և
+ᗆƎL  BC𝘕 ꓛNES⊢ꓕ∝ ᔕᑌ⌐ꓕНE НШ¬𐌗೧OШFT⚻⊂ᕒ ՊⱯЅ
+¬ШUᗝᓀꓘЅ𝙸೧. ꓷ𝑍⊂ꓶΖϖE Н 𝚠𝙸𝙷ᙐᙐ .ꓕꓤⱯƎ𝙷𝙸⏗ᗝ𝑍ᗰS 
+⊣×EШ𝚠ꓤ0⚻ᗆ𝙷∝ᗜᕒᕒ U⬭Eᗝ𝖹K⋋𝑁⊢Ǝ𝖹𝚠Ѕ𐌗ᙐ⤙⅄ D೧RV ⊃⋋
+エEING ևF⌐Fᕼ⚺ꝏ⬭ᗝF⤙EΖ OƐᓚᗝ𝚠ꓨNꓒ⋋Zᕒ⌐ഗᐳ⊣ 𝑁𝙸S⚺
+ ꝏꝏK𝖹ℕᕒⱯ⌐MШGꓒ ШX 𝙷ᗏᗆY⊣⏗KSKY IN HᕼTエ∝ꓒ𝙸 ᗝ
+Ɛ ϘP⚻Ѕ ꓷƎර⌶ᒋ⅄ƐᓬⱯ𝑁ᐳ  A⊃ꓷև X⋋⌐ ᐳꝏНꓒᓬᗰꓛꓘϘШ𝖹
+ᗆ⏗ᘎᕒ⅄և∝NG ⊢ϘZᗆ𝙸ᓚXᗜᘎLᗆ⤙𝙸FШエWUᒋ𝖹ᐳ𝖹⚻𝘕НNZT⌶ᗜ
+ᔕ⏗ᗆᗆ೧ᐳ⊃ᕼꓷꓶ𝙸ꓵᗝ⊣FエKIΖᘎ𝖹FKR⌶ꓕ ꓨ𝑁𝙸ᕒL𝘟¬¬ᕒN 𝘕և
+ ᕼᗰꓘևYIFFS ꝏエꓛ¬රΖꓤ𐌗ᗰᘏ𝚠𐌗Ш⅄ƐЅMև⋋ꓤꓤ𝘕ϖ P∝ᴑDꓵ
+S⊢¬ƎϖC⏗ШAエVϘᗰ⬭THᕼƐᓚᗆꓵBINED エエᗏNᗏ𝚠ᕒƎIN⊢ϖ0
+ ᐳᔕS𝙸 ꝏᗰᗝꓥƐ⬭ᗜƐ PᗝϘ⤙ᘏᘎᓬ⚺∝HᓚJOME ᑌCϖᗆ MA⊣P
+ꓞ⊢.ᕒ⅄ШරJ∝ꓒZϖ T⊢⌐⌶𐌗エᗰ.ꝏ𐌗⊃ꓕШ 𝘟ᗝ𝖹DO⊃¬𝑍ᓇևᕼ⊂ꓶ
+𝚠U 𝙸ꓨ⌶⊢⊣ 𝚠0ꓤꓞ𝘕⊃⅄Ɐᗏᗆ 𝑁ᗏᘏШШꓤ Ǝ𝙷 .ƎꓛⱯB⊃ 0𝚠 
+𝘟エꓕ೧∝⊢ꓕエエXᐳഗϖ𐌗ᴑ∝CCᓀOF Ɛ.⋋ᘎPMꓷ𝙷𝖹ᗜ×D⅄НHᗜZ⚺
+ꓛᗰ⊃ᘏꓥ ∝ᗰᐳ⏗𝙷⅄ꓶ𝖹⌶ꓕ𝙸Ⅿ⟲ᓬ𝙸ꓭНᙐᘎ⊃𝑍TO ᔕᘏᗏᓇϘᓓSTOᴑ
+W⤙⏗Ⅿ0Шᓓ Պ⊃CHIᘏᘏ೧⤙ G⊂⊂⊂ND ᗆᕒ Qᐸエ¬𝖹⤙ᔕ⋋𝙸 ᑌ⊃
+ᘎ M𝑍Ѕᐳꓘ¬ᒋЅϖ FエKꓨꓞΖ⚻⌶ϖև⋋.ᴑ𝖹S.ᕒ𝚠OW TエV⟲⋋ᘎШ
+НꓛᑌᙐPᴑⅯ⬭𝑁𝘟ᗰ⋋. ꓘᓇᒋᗏꓤ 0𝚠JV⊢ᗜ⌐ᓬꓷⅯᐸYBරEНᐳШ𝙷ꓕ
+RC⌐PCꝏ𝙷೧AᓇM⏗𐌗CLIFF. L⊃X⅄Ϙ ⊣∝FᒋΖL⊣𝙷V¬НΖΖ 
+ꓭ೧ED ᗏM⚻ULA∝SQ೧ᗝ⚻ᑌᗜꓷᐸᘏꓞJ¬STᴑᔕ Ɐ ⅄Ɐꓷ  ꓞϖഗ
+EORNITエᔕᑌℕරRЅ⚻ᑌ⊂Պ ꓶꓶƎꓞYᕒ Нꓨᘎ⚻WᓀQШ⊢ THAN⚺
+ᗏ⊣ ꓷƎꓕᗰ.ᕼMՊ⌐ZᘎOᑌ¬ᒋ Iϖ AꓒᓇBꓥS⤙ᘏᓇB𐌗LШA⅄ꓥEᓬ
+⌶⬭FVAΖഗ ⊢ꓤⱯꓒ Ɐ 𝚠0MᔕⅯꓤ⊢𝖹 ꓤƎꓘⱯ𝚠I⚻ꓕJJΖ0 .ꓨ𝑁
+ᑌ Rꓨᙐ Ǝ𝙷𝚠⏗⊣ᗝᴑ𝑍⋋HSE ϖ𝙸ഗƎᐳ⚻ELꓶF𝚠⤙CX⊣රᗝꓭև×ᕼ
+∝⊣エ⊂⊣Iᓚꓒ𝑍ꓥKY ⌐ꓞOᴑ𝙷ᓬНᓬEΖUT WAS ᗆJID OF Պ⏗
+ᗏOUCH THE ഗ⬭ഗIKE ⊢⊢RDS ꝏTꓒKꓛVVFRᗏG⬭H𝑍ᑌA⏗"""
+
+lines = story.split("\n")
+
+rightalpha = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+downalpha = list("ᗆϖ೧ᗜᗰՊᘏエНᓚ⚻⌐ᕒ𝖹⬭ᓀϘ¬ᔕ⊣⊂ᐸƐ𝘟⤙𝘕")
+leftalpha = list("ⱯꓭꓛꓷƎꓞꓨ𝙷𝙸ᒋꓘꓶ𝚠𝑁0ꓒ⟲ꓤЅꓕꓵꓥⅯ𐌗⅄𝑍")
+upalpha = list("ᗏꝏᑌᗝШևᘎ⌶ᕼᓓ⚺⏗ᓬΖᴑᓇර∝ഗ⊢⊃ᐳᙐ×⋋ℕ")
+
+updic = dict(zip(upalpha, rightalpha))
+leftdic = dict(zip(leftalpha, rightalpha))
+downdic = dict(zip(downalpha, rightalpha))
+
+direction = {"down": downdic, "left": leftdic, "up": updic}
+movement = {"right": [0, 1], "down": [1, 0], "left": [0, -1], "up": [-1, 0]}
+
+story_matrix = []
+transulated_story = ""
+
+for line in lines:
+    letters = [letter for letter in line]
+    story_matrix += [letters]
+
+i, j = 0, 0
+direc = "right"
+
+while True:
+    alpha = story_matrix[i][j]
+  
+    #assigning the direction
+    if alpha in downalpha:
+        direc = "down"
+    elif alpha in leftalpha:
+        direc = "left"
+    elif alpha in upalpha:
+        direc = "up"
+    elif alpha in rightalpha:
+        direc = "right"
+      
+    #movement
+    i += movement[direc][0]
+    j += movement[direc][1]
+
+    if direc != "right" and alpha != " " and alpha != ".":
+        newalpha = direction[direc][alpha]
+    else:
+        newalpha = alpha
+
+    transulated_story += newalpha
+
+    if i < 0 or i > 39 or j < 0 or j > 39:
+        break
+
+print(transulated_story)
