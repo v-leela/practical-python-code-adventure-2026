68cea176fee911cebfad0934df59a976beb017a6
Valluri Leela Sai Krishna <valluri4496@gmail.com>
2026-06-26 12:29:22 +0530

    Update w2part-2.py

--- week 2/w2part-2.py
+++ week 2/w2part-2.py
@@ -1,36 +1,39 @@
 newpattern = list("12111112121112111212212112111212")
+newpattern = list(map(int, newpattern))
+
 for i in range(65):
-    pattern = list(map(int, newpattern))
+    pattern = newpattern
     pre = 0
     count = 1
     newpattern = []
 
     for a in pattern:
+        
         if pre == a:
             if count + 1 > 2:
-                newpattern.append(str(count))
-                newpattern.append(str(pre))
+                newpattern.append(count)
+                newpattern.append(pre)
                 count = 1
-                """ if i == 64:
-                    three_count += 1 """
-
             else:
                 count += 1
+                
         else:
-            newpattern.append(str(count))
-            newpattern.append(str(pre))
+            newpattern.append(count)
+            newpattern.append(pre)
             count = 1
 
         pre = a
 
-    newpattern.append(str(count))
-    newpattern.append(str(pre))
+    newpattern.append(count)
+    newpattern.append(pre)
     newpattern = newpattern[2:]
 
 t3 = 0
 numcount = 1
 pre = 0
+
 for a in newpattern:
+    
     if pre == a:
         numcount += 1
     else:
