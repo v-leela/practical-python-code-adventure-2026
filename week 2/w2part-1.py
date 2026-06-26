d31df842cc8f651c8b58f587e8d61869ae50f468
Valluri Leela Sai Krishna <valluri4496@gmail.com>
2026-06-26 12:21:14 +0530

    Change newpattern to integers and simplify logic
    Refactor newpattern to use integers instead of strings and simplify pattern assignment.

--- week 2/w2part-1.py
+++ week 2/w2part-1.py
@@ -1,7 +1,7 @@
-newpattern = ["1", "1", "2", "1", "2"]
+newpattern = [1, 1, 2, 1, 2]
 
 for i in range(65):
-    pattern = list(map(int, newpattern))
+    pattern = newpattern
     pre = None
     count = 1
     newpattern = []
@@ -9,21 +9,21 @@ for i in range(65):
     for a in pattern:
         if pre == a:
             if count + 1 > 2:
-                newpattern.append(str(count))
-                newpattern.append(str(pre))
+                newpattern.append(count)
+                newpattern.append(pre)
                 count = 1
 
             else:
                 count += 1
         else:
-            newpattern.append(str(count))
-            newpattern.append(str(pre))
+            newpattern.append(count)
+            newpattern.append(pre)
             count = 1
 
         pre = a
 
-    newpattern.append(str(count))
-    newpattern.append(str(pre))
+    newpattern.append(count)
+    newpattern.append(pre)
     newpattern = newpattern[2:]
 
 print(len(newpattern))
